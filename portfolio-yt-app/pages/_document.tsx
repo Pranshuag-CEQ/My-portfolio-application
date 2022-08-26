c00885e7cd50b70931286df0c88796caa2e5f387
Pranshu Agrawal <112094655+Pranshuag11@users.noreply.github.com>
2022-08-26 06:08:29 +0530

    Finish building portfolio app

---
+++ portfolio-yt-app/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+
+export default function() {
+  return (
+    <Html className="dark">
+      <Head>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
